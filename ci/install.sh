df85298c5a42a764805c2a24a64f2d08049404e9
Jonas Schievink <jonasschievink@gmail.com>
2017-09-22 20:20:13 +0200

    Pin Travis to Xargo 0.3.8

--- ci/install.sh
+++ ci/install.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 main() {
     case $TARGET in
         thumbv*-none-eabi*)
-            cargo install --list | grep xargo || \
-                cargo install xargo
+            cargo install --list | grep 'xargo v0.3.8' || \
+                cargo install xargo --vers 0.3.8
             rustup component list | grep 'rust-src.*installed' || \
                 rustup component add rust-src
             ;;
