a7d8084aeea2f0697c9f93c2789773ea79d4db0c
Jorge Aparicio <jorge@japaric.io>
2018-11-03 18:58:25 +0100

    ci: install mdbook

--- ci/install.sh
+++ ci/install.sh
@@ -8,8 +8,10 @@ main() {
     mkdir qemu
     curl -L https://github.com/japaric/qemu-bin/raw/master/14.04/qemu-system-arm-2.12.0 > qemu/qemu-system-arm
     chmod +x qemu/qemu-system-arm
+
+    # install mdbook
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- --git rust-lang-nursery/mdbook --tag v0.2.1
 }
 
-if [ $TRAVIS_BRANCH != master ] || [ $TRAVIS_PULL_REQUEST != false ]; then
-    main
-fi
+main
