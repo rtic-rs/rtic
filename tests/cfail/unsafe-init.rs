d538f5b17cf0ad482cd803d7fbaada3349d6485e
Jorge Aparicio <jorge@japaric.io>
2019-04-21 20:20:33 +0200

    test RFC 147

---
+++ tests/cfail/unsafe-init.rs
@@ -0,0 +1,15 @@
+#![no_main]
+#![no_std]
+
+extern crate lm3s6965;
+extern crate panic_halt;
+extern crate rtfm;
+
+use rtfm::app;
+
+#[app(device = lm3s6965)]
+const APP: () = {
+    #[init]
+    unsafe fn init(_: init::Context) {}
+    //~^ ERROR `init` must have type signature `fn(init::Context) [-> init::LateResources]`
+};
