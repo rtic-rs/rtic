c7e63852341840f67f8b6164f4aea458960ebd56
Per Lindgren <per.lindgren@ltu.se>
2020-10-24 19:47:38 +0200

    extern examples added

---
+++ examples/extern_binds.rs
@@ -0,0 +1,48 @@
+//! examples/extern_binds.rs
+
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+use cortex_m_semihosting::hprintln;
+use panic_semihosting as _;
+
+fn foo(_: app::foo::Context) {
+    hprintln!("foo called").ok();
+}
+
+// `examples/interrupt.rs` rewritten to use `binds`
+#[rtic::app(device = lm3s6965)]
+mod app {
+    use crate::foo;
+    use cortex_m_semihosting::{debug, hprintln};
+    use lm3s6965::Interrupt;
+
+    #[init]
+    fn init(_: init::Context) -> init::LateResources {
+        rtic::pend(Interrupt::UART0);
+
+        hprintln!("init").unwrap();
+
+        init::LateResources {}
+    }
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        hprintln!("idle").unwrap();
+
+        rtic::pend(Interrupt::UART0);
+
+        debug::exit(debug::EXIT_SUCCESS);
+
+        loop {
+            cortex_m::asm::nop();
+        }
+    }
+
+    extern "C" {
+        #[task(binds = UART0)]
+        fn foo(_: foo::Context);
+    }
+}
