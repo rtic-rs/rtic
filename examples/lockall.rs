1ba4d1aacaf4a18b90ac8536f4b5a3bcfe716e3b
Per Lindgren <per.lindgren@ltu.se>
2021-10-18 17:59:09 +0200

    api test, all implemented, TODO hide priority?

--- examples/lockall.rs
+++ examples/lockall.rs
@@ -1,13 +1,13 @@
-//! examples/lock.rs
+//! examples/lockall.rs
 
-// #![deny(unsafe_code)]
-// #![deny(warnings)]
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_main]
 #![no_std]
 
 use panic_semihosting as _;
 
-#[rtic::app(device = lm3s6965, dispatchers = [GPIOA])]
+#[rtic::app(device = lm3s6965, dispatchers = [GPIOA, GPIOB, GPIOC])]
 mod app {
     use cortex_m_semihosting::{debug, hprintln};
 
@@ -30,44 +30,31 @@ mod app {
     // when omitted priority is assumed to be `1`
     #[task(shared = [a, b])]
     fn foo(mut c: foo::Context) {
-        static mut X: Option<&'static mut u32> = None;
-        static mut Y: u32 = 0;
-        let _ = hprintln!("before lock");
         c.shared.lock(|s| {
-            let _ = hprintln!("in lock");
-            let _ = hprintln!("here {}, {}", s.a, s.b);
+            hprintln!("foo: a = {}, b = {}", s.a, s.b).ok();
             *s.a += 1;
-
-            // soundness check
-            // c.shared.lock(|s| {}); // borrow error
-            // c.shared.a.lock(|s| {}); // borrow error
-
-            unsafe {
-                X = Some(&mut Y);
-                // X = Some(s.a); // lifetime issue
-                // X = Some(&mut *s.a); // lifetime issue
-                // X = Some(&'static mut *s.a); // not rust
-            }
-            let _ = hprintln!("here {}, {}", s.a, s.b);
+            bar::spawn().unwrap();
+            baz::spawn().unwrap();
+            hprintln!("still in foo::lock").ok();
         });
-        // the lower priority task requires a critical section to access the data
-        // c.shared.shared.lock(|shared| {
-        //     // data can only be modified within this critical section (closure)
-        //     *shared += 1;
-
-        //     // bar will *not* run right now due to the critical section
-        //     bar::spawn().unwrap();
-
-        //     hprintln!("B - shared = {}", *shared).unwrap();
-
-        //     // baz does not contend for `shared` so it's allowed to run now
-        //     baz::spawn().unwrap();
-        // });
-
-        // // critical section is over: bar can now start
-
-        // hprintln!("E").unwrap();
-
+        hprintln!("still in foo").ok();
         debug::exit(debug::EXIT_SUCCESS); // Exit QEMU simulator
+    }
+
+    #[task(priority = 2, shared = [a])]
+    fn bar(mut c: bar::Context) {
+        // the higher priority task does still need a critical section
+        let a = c.shared.lock(|s| {
+            *s.a += 1;
+            // *s.b += 1; `b` not accessible
+            *s.a
+        });
+
+        hprintln!("bar: a = {}", a).unwrap();
+    }
+
+    #[task(priority = 3)]
+    fn baz(_: baz::Context) {
+        hprintln!("baz").unwrap();
     }
 }
