yoki31/cortex-m-rtic

wip Priority leakage

macros/src/codegen/shared_resources.rs
@@ -85,10 +85,10 @@ pub fn codegen(
             mod_app.push(util::impl_mutex(
                 extra,
                 cfgs,
-                true,
-                &name,
+                quote!(shared_resources::#name),
                 quote!(#ty),
                 ceiling,
+                quote!(self.priority()),
                 ptr,
             ));
         }

rtic-rs/rtic

macros/src/codegen/shared_resources.rs
@@ -87,10 +87,10 @@ pub fn codegen(
             mod_app.push(util::impl_mutex(
                 extra,
                 cfgs,
-                true,
-                &shared_name,
+                quote!(shared_resources::#shared_name),
                 quote!(#ty),
                 ceiling,
+                quote!(self.priority()),
                 ptr,
             ));
         }