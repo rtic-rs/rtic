c9aa5003846dbce42bd576413338f56de5a24da3
Finomnis <finomnis@gmail.com>
2023-12-27 12:08:09 +0100

    Rework timer_queue and monotonic architecture

--- rtic-time/src/monotonic.rs
+++ rtic-time/src/monotonic.rs
@@ -1,5 +1,9 @@
 //! A monotonic clock / counter definition.
 
+use core::marker::PhantomData;
+
+use crate::TimeoutError;
+
 /// # A monotonic clock / counter definition.
 ///
 /// ## Correctness
@@ -25,200 +29,148 @@ pub trait Monotonic {
     /// The type for duration, defining an duration of time.
     ///
     /// **Note:** In all APIs in RTIC that use duration from this monotonic, this type will be used.
-    type Duration;
+    type Duration: Copy;
 
     /// Get the current time.
     fn now() -> Self::Instant;
 
-    /// Set the compare value of the timer interrupt.
-    ///
-    /// **Note:** This method does not need to handle race conditions of the monotonic, the timer
-    /// queue in RTIC checks this.
-    fn set_compare(instant: Self::Instant);
+    /// Delay for some duration of time.
+    async fn delay(duration: Self::Duration);
 
-    /// This method used to be required by an errata workaround
-    /// for the nrf52 family, but it has been disabled as the
-    /// workaround was erroneous.
-    #[deprecated(
-        since = "1.2.0",
-        note = "this method is erroneous and has been disabled"
-    )]
-    fn should_dequeue_check(_: Self::Instant) -> bool {
-        panic!("This method should not be used as it is erroneous.")
+    /// Delay to some specific time instant.
+    async fn delay_until(instant: Self::Instant);
+
+    /// Timeout at a specific time.
+    async fn timeout_at<F: core::future::Future>(
+        instant: Self::Instant,
+        future: F,
+    ) -> Result<F::Output, TimeoutError>;
+
+    /// Timeout after a specific duration.
+    async fn timeout_after<F: core::future::Future>(
+        duration: Self::Duration,
+        future: F,
+    ) -> Result<F::Output, TimeoutError>;
+
+    /// A duration which represents the specified amount of milliseconds,
+    /// rounded up.
+    ///
+    /// Required for embedded-hal implementations.
+    fn duration_millis_at_least(ms: u32) -> Self::Duration;
+
+    /// A duration which represents the specified amount of microseconds,
+    /// rounded up.
+    ///
+    /// Required for embedded-hal implementations.
+    fn duration_micros_at_least(us: u32) -> Self::Duration;
+
+    /// A duration which represents the specified amount of nanoseconds,
+    /// rounded up.
+    ///
+    /// Required for embedded-hal implementations.
+    fn duration_nanos_at_least(ns: u32) -> Self::Duration;
+}
+
+struct MonotonicWrapper<Mono: Monotonic> {
+    _m: PhantomData<Mono>,
+}
+
+impl<T: Monotonic> Monotonic for MonotonicWrapper<T> {
+    const ZERO: T::Instant = T::ZERO;
+
+    const TICK_PERIOD: T::Duration = T::TICK_PERIOD;
+
+    type Instant = T::Instant;
+
+    type Duration = T::Duration;
+
+    fn now() -> Self::Instant {
+        T::now()
     }
 
-    /// Clear the compare interrupt flag.
-    fn clear_compare_flag();
+    async fn delay(duration: Self::Duration) {
+        T::delay(duration).await
+    }
 
-    /// Pend the timer's interrupt.
-    fn pend_interrupt();
+    async fn delay_until(instant: Self::Instant) {
+        T::delay_until(instant).await
+    }
 
-    /// Optional. Runs on interrupt before any timer queue handling.
-    fn on_interrupt() {}
+    async fn timeout_at<F: core::future::Future>(
+        instant: Self::Instant,
+        future: F,
+    ) -> Result<F::Output, TimeoutError> {
+        T::timeout_at(instant, future).await
+    }
 
-    /// Optional. This is used to save power, this is called when the timer queue is not empty.
-    ///
-    /// Enabling and disabling the monotonic needs to propagate to `now` so that an instant
-    /// based of `now()` is still valid.
-    ///
-    /// NOTE: This may be called more than once.
-    fn enable_timer() {}
+    async fn timeout_after<F: core::future::Future>(
+        duration: Self::Duration,
+        future: F,
+    ) -> Result<F::Output, TimeoutError> {
+        T::timeout_after(duration, future).await
+    }
 
-    /// Optional. This is used to save power, this is called when the timer queue is empty.
-    ///
-    /// Enabling and disabling the monotonic needs to propagate to `now` so that an instant
-    /// based of `now()` is still valid.
-    ///
-    /// NOTE: This may be called more than once.
-    fn disable_timer() {}
+    fn duration_millis_at_least(ms: u32) -> Self::Duration {
+        T::duration_millis_at_least(ms)
+    }
+
+    fn duration_micros_at_least(us: u32) -> Self::Duration {
+        T::duration_micros_at_least(us)
+    }
+
+    fn duration_nanos_at_least(ns: u32) -> Self::Duration {
+        T::duration_nanos_at_least(ns)
+    }
 }
 
-/// Creates impl blocks for `embedded_hal::delay::DelayUs`,
-/// based on `fugit::ExtU64Ceil`.
-#[macro_export]
-macro_rules! embedded_hal_delay_impl_fugit64 {
-    ($t:ty) => {
-        impl ::embedded_hal::delay::DelayNs for $t {
-            fn delay_ns(&mut self, ns: u32) {
-                use ::fugit::ExtU64Ceil;
-
-                let now = Self::now();
-                let mut done = now + u64::from(ns).nanos_at_least();
-                if now != done {
-                    // Compensate for sub-tick uncertainty
-                    done += Self::TICK_PERIOD;
-                }
-
-                while Self::now() < done {}
-            }
-
-            fn delay_us(&mut self, us: u32) {
-                use ::fugit::ExtU64Ceil;
-
-                let now = Self::now();
-                let mut done = now + u64::from(us).micros_at_least();
-                if now != done {
-                    // Compensate for sub-tick uncertainty
-                    done += Self::TICK_PERIOD;
-                }
-
-                while Self::now() < done {}
-            }
-
-            fn delay_ms(&mut self, ms: u32) {
-                use ::fugit::ExtU64Ceil;
-
-                let now = Self::now();
-                let mut done = now + u64::from(ms).millis_at_least();
-                if now != done {
-                    // Compensate for sub-tick uncertainty
-                    done += Self::TICK_PERIOD;
-                }
-
-                while Self::now() < done {}
-            }
+impl<T: Monotonic> embedded_hal::delay::DelayNs for MonotonicWrapper<T> {
+    fn delay_ns(&mut self, ns: u32) {
+        let now = Self::now();
+        let mut done = now + Self::duration_nanos_at_least(ns);
+        if now != done {
+            // Compensate for sub-tick uncertainty
+            done = done + Self::TICK_PERIOD;
         }
-    };
+
+        while Self::now() < done {}
+    }
+
+    fn delay_us(&mut self, us: u32) {
+        let now = Self::now();
+        let mut done = now + Self::duration_micros_at_least(us);
+        if now != done {
+            // Compensate for sub-tick uncertainty
+            done = done + Self::TICK_PERIOD;
+        }
+
+        while Self::now() < done {}
+    }
+
+    fn delay_ms(&mut self, ms: u32) {
+        let now = Self::now();
+        let mut done = now + Self::duration_millis_at_least(ms);
+        if now != done {
+            // Compensate for sub-tick uncertainty
+            done = done + Self::TICK_PERIOD;
+        }
+
+        while Self::now() < done {}
+    }
 }
 
-/// Creates impl blocks for `embedded_hal_async::delay::DelayUs`,
-/// based on `fugit::ExtU64Ceil`.
-#[macro_export]
-macro_rules! embedded_hal_async_delay_impl_fugit64 {
-    ($t:ty) => {
-        impl ::embedded_hal_async::delay::DelayNs for $t {
-            #[inline]
-            async fn delay_ns(&mut self, ns: u32) {
-                use ::fugit::ExtU64Ceil;
-                Self::delay(u64::from(ns).nanos_at_least()).await;
-            }
+impl<T: Monotonic> embedded_hal_async::delay::DelayNs for MonotonicWrapper<T> {
+    #[inline]
+    async fn delay_ns(&mut self, ns: u32) {
+        Self::delay(Self::duration_nanos_at_least(ns)).await;
+    }
 
-            #[inline]
-            async fn delay_us(&mut self, us: u32) {
-                use ::fugit::ExtU64Ceil;
-                Self::delay(u64::from(us).micros_at_least()).await;
-            }
+    #[inline]
+    async fn delay_us(&mut self, us: u32) {
+        Self::delay(Self::duration_micros_at_least(us)).await;
+    }
 
-            #[inline]
-            async fn delay_ms(&mut self, ms: u32) {
-                use ::fugit::ExtU64Ceil;
-                Self::delay(u64::from(ms).millis_at_least()).await;
-            }
-        }
-    };
-}
-
-/// Creates impl blocks for `embedded_hal::delay::DelayUs`,
-/// based on `fugit::ExtU32Ceil`.
-#[macro_export]
-macro_rules! embedded_hal_delay_impl_fugit32 {
-    ($t:ty) => {
-        impl ::embedded_hal::delay::DelayNs for $t {
-            fn delay_ns(&mut self, ns: u32) {
-                use ::fugit::ExtU32Ceil;
-
-                let now = Self::now();
-                let mut done = now + ns.nanos_at_least();
-                if now != done {
-                    // Compensate for sub-tick uncertainty
-                    done += Self::TICK_PERIOD;
-                }
-
-                while Self::now() < done {}
-            }
-
-            fn delay_us(&mut self, us: u32) {
-                use ::fugit::ExtU32Ceil;
-
-                let now = Self::now();
-                let mut done = now + us.micros_at_least();
-                if now != done {
-                    // Compensate for sub-tick uncertainty
-                    done += Self::TICK_PERIOD;
-                }
-
-                while Self::now() < done {}
-            }
-
-            fn delay_ms(&mut self, ms: u32) {
-                use ::fugit::ExtU32Ceil;
-
-                let now = Self::now();
-                let mut done = now + ms.millis_at_least();
-                if now != done {
-                    // Compensate for sub-tick uncertainty
-                    done += Self::TICK_PERIOD;
-                }
-
-                while Self::now() < done {}
-            }
-        }
-    };
-}
-
-/// Creates impl blocks for `embedded_hal_async::delay::DelayUs`,
-/// based on `fugit::ExtU32Ceil`.
-#[macro_export]
-macro_rules! embedded_hal_async_delay_impl_fugit32 {
-    ($t:ty) => {
-        impl ::embedded_hal_async::delay::DelayNs for $t {
-            #[inline]
-            async fn delay_ns(&mut self, ns: u32) {
-                use ::fugit::ExtU32Ceil;
-                Self::delay(ns.nanos_at_least()).await;
-            }
-
-            #[inline]
-            async fn delay_us(&mut self, us: u32) {
-                use ::fugit::ExtU32Ceil;
-                Self::delay(us.micros_at_least()).await;
-            }
-
-            #[inline]
-            async fn delay_ms(&mut self, ms: u32) {
-                use ::fugit::ExtU32Ceil;
-                Self::delay(ms.millis_at_least()).await;
-            }
-        }
-    };
+    #[inline]
+    async fn delay_ms(&mut self, ms: u32) {
+        Self::delay(Self::duration_millis_at_least(ms)).await;
+    }
 }
---
+++ rtic-time/src/timer_queue/backend.rs
@@ -0,0 +1,46 @@
+use super::TimerQueueTicks;
+
+/// # A monotonic clock / counter definition.
+///
+/// ## Correctness
+///
+/// The trait enforces that proper time-math is implemented between `Instant` and `Duration`. This
+/// is a requirement on the time library that the user chooses to use.
+pub trait TimerQueueBackend {
+    /// The type for ticks
+    type Ticks: TimerQueueTicks;
+
+    /// Get the current time.
+    fn now() -> Self::Ticks;
+
+    /// Set the compare value of the timer interrupt.
+    ///
+    /// **Note:** This method does not need to handle race conditions of the monotonic, the timer
+    /// queue in RTIC checks this.
+    fn set_compare(instant: Self::Ticks);
+
+    /// Clear the compare interrupt flag.
+    fn clear_compare_flag();
+
+    /// Pend the timer's interrupt.
+    fn pend_interrupt();
+
+    /// Optional. Runs on interrupt before any timer queue handling.
+    fn on_interrupt() {}
+
+    /// Optional. This is used to save power, this is called when the timer queue is not empty.
+    ///
+    /// Enabling and disabling the monotonic needs to propagate to `now` so that an instant
+    /// based of `now()` is still valid.
+    ///
+    /// NOTE: This may be called more than once.
+    fn enable_timer() {}
+
+    /// Optional. This is used to save power, this is called when the timer queue is empty.
+    ///
+    /// Enabling and disabling the monotonic needs to propagate to `now` so that an instant
+    /// based of `now()` is still valid.
+    ///
+    /// NOTE: This may be called more than once.
+    fn disable_timer() {}
+}
