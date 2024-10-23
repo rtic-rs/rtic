7f94e5844f818ddb7d788f515eb2e6311be18d6e
Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>
2024-10-23 18:50:12 +0200

    Prepare for new riscv ecosystem

--- rtic/src/export/slic.rs
+++ rtic/src/export/slic.rs
@@ -1,7 +1,10 @@
 pub use riscv_slic::{lock, pend, run, InterruptNumber};
 
-#[cfg(all(feature = "riscv-slic", not(feature = "riscv-clint-backend")))]
-compile_error!("Building for the riscv-slic, but 'riscv-clint-backend' not selected");
+#[cfg(all(
+    feature = "riscv-slic",
+    not(any(feature = "riscv-clint-backend", feature = "riscv-mecall-backend"))
+))]
+compile_error!("Building for the riscv-slic, but no compatible backend selected");
 
 /// USE CASE RE-EXPORTS: needed for SLIC-only
 pub use riscv_slic::{self, clear_interrupts, codegen, set_interrupts, set_priority};
