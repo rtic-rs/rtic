6b021de4a0f0400d21453d9913aba57c6b395f1c
Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>
2025-01-28 12:38:07 +0100

    Add mecall backend

--- rtic/src/export/slic.rs
+++ rtic/src/export/slic.rs
@@ -7,16 +7,16 @@ pub use riscv_slic::{lock, pend, run, InterruptNumber};
 compile_error!("Building for the riscv-slic, but no compatible backend selected");
 
 /// USE CASE RE-EXPORTS: needed for SLIC-only
-pub use riscv_slic::{self, clear_interrupts, codegen, set_interrupts, set_priority};
+pub use riscv_slic::{self, codegen, set_priority};
 
 pub mod interrupt {
+    #[inline]
     pub fn disable() {
         riscv_slic::disable();
-        riscv_slic::clear_interrupts();
     }
 
+    #[inline]
     pub unsafe fn enable() {
-        riscv_slic::set_interrupts();
         riscv_slic::enable();
     }
 }
