b5db43550185c2acd62d3f27bc89f2f24b4fbb22
datdenkikniet <jcdra1@gmail.com>
2025-03-16 12:46:23 +0100

    rtic-sync: introduce loom compat layer and apply it to `channel`

--- rtic-sync/src/lib.rs
+++ rtic-sync/src/lib.rs
@@ -1,6 +1,6 @@
 //! Synchronization primitives for asynchronous contexts.
 
-#![no_std]
+#![cfg_attr(not(loom), no_std)]
 #![deny(missing_docs)]
 
 #[cfg(feature = "defmt-03")]
@@ -11,6 +11,11 @@ pub mod channel;
 pub use portable_atomic;
 pub mod signal;
 
+mod unsafecell;
+
 #[cfg(test)]
 #[macro_use]
 extern crate std;
+
+#[cfg(loom)]
+mod loom_cs;
