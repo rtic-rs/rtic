0615841cb8fe3c4aa65c79d05d5183298a12a39e
Eli Hastings <eli@seagen.io>
2025-03-21 16:27:58 +0000

    Don't use interrupts reserved by esp-hal
    Excluding the reserved interrupts fixes the task priorities. I also
    considered refactoring `rtic/src/export/riscv_esp32c6.rs` to use esp-hal
    instead of esp32c6 directly as it has code to do all the pointer
    wrangling itself, but decided against it for now. It might be nice to
    refactor both esp implentations to use it though.

--- rtic/src/export/riscv_esp32c6.rs
+++ rtic/src/export/riscv_esp32c6.rs
@@ -1,5 +1,5 @@
 pub use esp32c6::{Interrupt, Peripherals};
-use esp32c6::{INTERRUPT_CORE0, INTPRI, PLIC_MX};
+use esp32c6::{INTERRUPT_CORE0, PLIC_MX};
 pub use riscv::interrupt;
 pub use riscv::register::mcause;
 
@@ -148,32 +148,14 @@ pub fn enable(int: Interrupt, prio: u8, cpu_int_id: u8) {
             .offset(int as isize)
             .write_volatile(cpu_int_id as u32);
 
-        match int {
-            Interrupt::FROM_CPU_INTR0
-            | Interrupt::FROM_CPU_INTR1
-            | Interrupt::FROM_CPU_INTR2
-            | Interrupt::FROM_CPU_INTR3 => {
-                // Set the interrupt's priority:
-                (*INTPRI::ptr())
-                    .cpu_int_pri(cpu_int_id as usize)
-                    .write(|w| w.bits(prio as u32));
+        // Set the interrupt's priority:
+        (*PLIC_MX::ptr())
+            .mxint_pri(cpu_int_id as usize)
+            .write(|w| w.bits(prio as u32));
 
-                // Finally, enable the CPU interrupt:
-                (*INTPRI::ptr())
-                    .cpu_int_enable()
-                    .modify(|r, w| w.bits((1 << cpu_int_id) | r.bits()));
-            }
-            _ => {
-                // Set the interrupt's priority:
-                (*PLIC_MX::ptr())
-                    .mxint_pri(cpu_int_id as usize)
-                    .write(|w| w.bits(prio as u32));
-
-                // Finally, enable the CPU interrupt:
-                (*PLIC_MX::ptr())
-                    .mxint_enable()
-                    .modify(|r, w| w.bits((1 << cpu_int_id) | r.bits()));
-            }
-        }
+        // Finally, enable the CPU interrupt:
+        (*PLIC_MX::ptr())
+            .mxint_enable()
+            .modify(|r, w| w.bits((1 << cpu_int_id) | r.bits()));
     }
 }
