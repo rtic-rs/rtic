a1eb285cacac9cdbc363fce9d0c0db14549c2a84
Oleksandr Babak <alexanderbabak@proton.me>
2025-05-15 15:00:23 +0200

    feat: add ui test

---
+++ rtic/ui/custom_section.rs
@@ -0,0 +1,18 @@
+#![no_main]
+
+#[rtic::app(device = lm3s6965)]
+mod app {
+    #[shared]
+    struct Shared {
+        #[link_section = ".custom_section"]
+        foo: (),
+    }
+
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(_cx: init::Context) -> (Shared, Local) {
+        (Shared { foo: () }, Local {})
+    }
+}
