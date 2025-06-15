c3f77bc67473c46ecd0b8c290f28f013ee4bdaca
Henrik Tjäder <henrik@tjaders.com>
2025-06-15 21:25:06 +0200

    ci: QEMU esp32c3: There is no kill like overkill

--- examples/esp32c3/runner.sh
+++ examples/esp32c3/runner.sh
@@ -33,6 +33,18 @@ sleep 3s
 echo q | nc -N 127.0.0.1 55555 >>"$tempdir"/$qemuoutputfile 2>&1
 # Output that will be compared must be printed to stdout
 
+sleep 0.1s
+# If still running, try again nicely
+pgrep -af "qemu-system.*" >/dev/null 2>&1 && echo q | nc -N 127.0.0.1 55555 >>"$tempdir"/$qemuoutputfile 2>&1
+
+# Ask a bit more firmly with SIGTERM
+pgrep -af "qemu-system.*" >/dev/null 2>&1 && kill $qemupid >/dev/null 2>&1
+
+pgrep -af "qemu-system.*" >/dev/null 2>&1 && sleep 0.1s >/dev/null 2>&1
+
+# Time to die
+pgrep -af "qemu-system.*" >/dev/null 2>&1 && kill -9 $qemupid >/dev/null 2>&1
+
 # Make boot phase silent, for debugging change, run with e.g.  $ `env DEBUGGING=true` cargo xtask....
 if [ -n "${DEBUGGING}" ]; then
   # Debugging: strip leading "I (xyz)" where xyz is an incrementing number, and esp_image specifics
