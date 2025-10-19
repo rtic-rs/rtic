a599039a4eaa12572fe9b9340e0df4bacd3db475
Albin Hedman <albin9604@gmail.com>
2025-10-19 23:56:59 +0200

    Fix example

--- examples/lm3s6965/examples/spawn_local.rs
+++ examples/lm3s6965/examples/spawn_local.rs
@@ -5,7 +5,7 @@ use panic_semihosting as _;
 
 #[rtic::app(device = lm3s6965, dispatchers = [SSI0])]
 mod app {
-    use cortex_m_semihosting::hprintln;
+    use cortex_m_semihosting::{debug, hprintln};
     use super::*;
 
     #[shared]
