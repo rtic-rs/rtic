5e658e4064e1f5ad67896cee25657f0fbb062c36
Albin Hedman <albin9604@gmail.com>
2025-10-19 19:50:27 +0200

    Move tests to rtic/ui

--- rtic/ui/spawn-local-from-init.rs
+++ rtic/ui/spawn-local-from-init.rs
@@ -1,9 +1,7 @@
 #![no_main]
 
-#[rtic_macros::mock_app(device = mock, dispatchers = [EXTI0])]
+#[rtic::app(device = lm3s6965, dispatchers = [SSI0])]
 mod app {
-    use super::*;
-
     #[shared]
     struct Shared {}
 
@@ -12,14 +10,10 @@ mod app {
 
     #[init]
     fn init(_cx: init::Context) -> (Shared, Local) {
+        foo::spawn().ok();
         (Shared {}, Local {})
     }
 
     #[task(priority = 1, local_task)]
     async fn foo(_cx: foo::Context) {}
-
-    #[task(priority = 2)]
-    async fn bar(cx: bar::Context) {
-        cx.local_spawner.foo().ok();
-    }
 }
