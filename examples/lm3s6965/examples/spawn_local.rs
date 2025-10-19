09598ad9e90776f3244e15147d8ff82966b9a558
Albin Hedman <albin9604@gmail.com>
2025-10-19 23:50:12 +0200

    Add exit to example

--- examples/lm3s6965/examples/spawn_local.rs
+++ examples/lm3s6965/examples/spawn_local.rs
@@ -25,18 +25,12 @@ mod app {
     async fn task1(cx: task1::Context) {
         hprintln!("Hello from task1!");
         cx.local_spawner.task2(Default::default()).unwrap();
-        cx.local_spawner.task3(Default::default()).unwrap();
     }
 
     #[task(priority = 1, local_task = true)]
     async fn task2(_cx: task2::Context, _nsns: NotSendNotSync) {
         hprintln!("Hello from task2!");
-    }
-
-    // The value of `local_task` may be omitted and is in that case interpreted as `true`
-    #[task(priority = 1, local_task)]
-    async fn task3(_cx: task3::Context, _nsns: NotSendNotSync) {
-        hprintln!("Hello from task3!");
+        debug::exit(debug::EXIT_SUCCESS); // Exit QEMU simulator
     }
 }
 
