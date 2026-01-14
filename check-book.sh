7b1c7e1889fc0570813f6ccd0f5c471b9aab86b9
Henrik Tjäder <henrik@tjaders.com>
2026-01-14 20:27:44 +0100

    CI: Linkchecking with lychee changed relative checks
    Exclude /$version links that are not yet populated
    during this building stage
    Add root-dir to handle relative links
    
    Newline added to the file
    
    The change in lychee introduced in
    https://github.com/lycheeverse/lychee/pull/1864#issuecomment-3368121638

--- check-book.sh
+++ check-book.sh
@@ -13,6 +13,6 @@ cp -r book/en/book/ book-target/book/en/
 cp LICENSE-* book-target/book/en
 cp -r target/doc/ book-target/api/
 
-lychee --offline --format detailed book-target/book/en/
+lychee -v --offline --format detailed --exclude '.*/\d{1}' --root-dir book-target book-target/book/en/
 
 rm -rf book-target/
