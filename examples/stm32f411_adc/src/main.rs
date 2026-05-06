2b84d5c2a3226b3e74dbf8eba6c3e6acb5f21a86
Henrik Tjäder <henrik@tjaders.com>
2026-05-06 08:09:36 +0200

    clippy: stm32f411_adc

--- examples/stm32f411_adc/src/main.rs
+++ examples/stm32f411_adc/src/main.rs
@@ -84,7 +84,7 @@ mod app {
         adc.calibrate();
 
         let pot_instance = Potentiometer {
-            analog_input: analog_input,
+            analog_input,
         };
 
         // Configure Button Pin for Interrupts
@@ -134,7 +134,7 @@ mod app {
     #[task(binds = EXTI0, local = [button, pot_instance], shared=[delayval, adc_module])]
     fn gpio_interrupt_handler(mut ctx: gpio_interrupt_handler::Context) {
         ctx.shared.delayval.lock(|del| {
-            *del = *del - 100_u32;
+            *del -= 100_u32;
             if *del < 200_u32 {
                 *del = 2000_u32;
             }
