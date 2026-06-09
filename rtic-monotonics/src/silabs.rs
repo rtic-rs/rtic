4bf81f70fd3959521391e83bee3312f9330b440f
Andres Vahter <andres@vahter.me>
2026-06-09 19:54:27 +0300

    silabs monotonics: add TIMER0 monotonic

--- rtic-monotonics/src/silabs.rs
+++ rtic-monotonics/src/silabs.rs
@@ -1,12 +1,18 @@
-//! [`Monotonic`](super::Monotonic) implementations for the SiLabs series of
-//! MCUs (EFR32, EFM32, ...).
+//! [`Monotonic`](super::Monotonic) implementations for the SiLabs EFR32/EFM32
+//! series.
 //!
-//! There are two monotonic implementations:
-//! * [rtcc]: uses the RTCC peripheral (32-bit), only supported on the EFR32MG22
-//! * [letimer]: uses the LETimer peripheral (24-bit)
+//! * [rtcc]: RTCC peripheral (32-bit).
+//! * [letimer]: LETimer (24-bit), low-frequency clock that runs in EM2+ deep sleep.
+//! * [timer]: TIMER0 (32-bit), high-frequency clock prescaled to a chosen tick rate.
 
 pub mod letimer;
 #[cfg(feature = "silabs-efr32mg22")]
 pub mod rtcc;
+#[cfg(any(
+    feature = "silabs-efr32mg24",
+    feature = "silabs-efr32fg25",
+    feature = "silabs-efr32mg26"
+))]
+pub mod timer;
 
 const NVIC_PRIO_BITS: u8 = 4;
