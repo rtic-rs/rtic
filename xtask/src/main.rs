dc1112d6e9b39fe0615c604141917eceb121cade
datdenkikniet <jcdra1@gmail.com>
2026-08-04 19:50:13 +0200

    Overhaul xtasks
    * Commands & arguments are now fully separate for packages and examples
    * Commands run "all sub-parts" by default. Arguments can only reduce the amount of things that are ran, not increase
    * `cargo xtask ci` is not as much of a behemoth
    * Reworked `xtask` to have next to no global arguments
    * Reworked all `xtask` code so that example-commands take a `Platform` and derive their target + backend + example files from that.
    * Remove `rayon` support from `xtask`: it's cool, but kind of useless (due to cargo blocking on builds). We can always add it back later
    * Stop running both `cargo check` and `cargo clippy`, just run `cargo clippy`
    * Make it simple to rework `xtask fmt` just format all `Cargo.toml`s instead of only specific packages. Not executed in this PR to keep the file noise low.
    * All examples now build by default, and only have extra features/arguments if there is no sane default to pick

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,53 +3,63 @@ mod build;
 mod cargo_command;
 mod run;
 
-use argument_parsing::{ExtraArguments, FormatOpt, Package, PackageOpt, TestOpt};
 use clap::Parser;
 use core::fmt;
 use std::{path::Path, str};
 
-use log::{error, log_enabled, trace, Level};
+use log::{log_enabled, trace, Level};
 
 use crate::{
-    argument_parsing::{BuildOrCheck, Cli, Commands, Platforms},
+    argument_parsing::{
+        BuildOrCheck, Cli, Commands, ExampleArgs, ExtraArguments, FormatOpt, Globals,
+        Hifive1Backend, OptionalPlatformSelector, Package, PackageOpt, Platform, QemuAndRun,
+    },
     build::init_build_dir,
     run::*,
 };
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub struct Target<'a> {
-    triple: &'a str,
+pub struct Target {
+    triple: &'static str,
     has_std: bool,
+    has_cas: bool,
 }
 
-impl<'a> Target<'a> {
-    pub const fn new(triple: &'a str, has_std: bool) -> Self {
-        Self { triple, has_std }
+impl Target {
+    pub const fn new(triple: &'static str, has_std: bool, has_cas: bool) -> Self {
+        Self {
+            triple,
+            has_std,
+            has_cas,
+        }
     }
 
-    pub fn triple(&self) -> &str {
+    pub fn triple(&self) -> &'static str {
         self.triple
     }
 
     pub fn has_std(&self) -> bool {
         self.has_std
     }
+
+    /// Get the feature needed to enable CAS for thsi target.
+    pub fn cas_feature(&self) -> Option<String> {
+        (!self.has_cas).then_some("portable-atomic/unsafe-assume-single-core".to_string())
+    }
 }
 
-impl core::fmt::Display for Target<'_> {
+impl core::fmt::Display for Target {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.triple)
     }
 }
 
-// x86_64-unknown-linux-gnu
-const _X86_64: Target = Target::new("x86_64-unknown-linux-gnu", true);
-const ARMV6M: Target = Target::new("thumbv6m-none-eabi", false);
-const ARMV7M: Target = Target::new("thumbv7m-none-eabi", false);
-const ARMV8MBASE: Target = Target::new("thumbv8m.base-none-eabi", false);
-const ARMV8MMAIN: Target = Target::new("thumbv8m.main-none-eabi", false);
-const RISCV32IMC: Target = Target::new("riscv32imc-unknown-none-elf", false);
-const RISCV32IMAC: Target = Target::new("riscv32imac-unknown-none-elf", false);
+const ARMV6M: Target = Target::new("thumbv6m-none-eabi", false, false);
+const ARMV7M: Target = Target::new("thumbv7m-none-eabi", false, true);
+const ARMV8MBASE: Target = Target::new("thumbv8m.base-none-eabi", false, true);
+const ARMV8MMAIN: Target = Target::new("thumbv8m.main-none-eabi", false, true);
+const RISCV32IMC: Target = Target::new("riscv32imc-unknown-none-elf", false, false);
+const RISCV32IMAC: Target = Target::new("riscv32imac-unknown-none-elf", false, true);
 
 fn main() -> anyhow::Result<()> {
     // if there's an `xtask` folder, we're *probably* at the root of this repo (we can't just
@@ -81,75 +91,6 @@ fn main() -> anyhow::Result<()> {
         "Stderr of child processes is inherited: {}",
         globals.stderr_inherited
     );
-    log::debug!("Partial features: {}", globals.partial);
-
-    let platform = globals.platform.unwrap_or_default();
-
-    let backend = if let Some(backend) = globals.backend {
-        backend
-    } else {
-        platform.default_backend()
-    };
-
-    // Check if the platform supports the backend
-    if platform.features(&backend).is_err() {
-        return Err(anyhow::anyhow!(
-            "platform {:?} does not support backend {:?}",
-            platform,
-            backend
-        ));
-    }
-    let examples_path = format!("./examples/{}/examples", platform.name());
-    let examples: Vec<_> = std::fs::read_dir(examples_path)?
-        .filter_map(|p| p.ok())
-        .map(|p| p.path())
-        .filter(|p| p.display().to_string().ends_with(".rs"))
-        .map(|path| path.file_stem().unwrap().to_str().unwrap().to_string())
-        .collect();
-
-    let example = globals.example.clone();
-    let exampleexclude = globals.exampleexclude.clone();
-
-    let examples_to_run = {
-        let mut examples_to_run = examples.clone();
-
-        if let Some(example) = example {
-            examples_to_run = examples.clone();
-            let examples_to_exclude = example.split(',').collect::<Vec<&str>>();
-            // From the list of all examples, remove all not listed as included
-            for ex in examples_to_exclude {
-                examples_to_run.retain(|x| *x.as_str() == *ex);
-            }
-        };
-
-        if let Some(example) = exampleexclude {
-            examples_to_run = examples.clone();
-            let examples_to_exclude = example.split(',').collect::<Vec<&str>>();
-            // From the list of all examples, remove all those listed as excluded
-            for ex in examples_to_exclude {
-                examples_to_run.retain(|x| *x.as_str() != *ex);
-            }
-        };
-
-        if log_enabled!(Level::Trace) {
-            trace!("All examples:\n{examples:?} number: {}", examples.len());
-            trace!(
-                "examples_to_run:\n{examples_to_run:?} number: {}",
-                examples_to_run.len()
-            );
-        }
-
-        if examples_to_run.is_empty() {
-            error!(
-                "\nThe example(s) you specified is not available. Available examples are:\
-                    \n{examples:#?}\n\
-             By default if example flag is emitted, all examples are tested.",
-            );
-            return Err(anyhow::anyhow!("Incorrect usage"));
-        } else {
-            examples_to_run
-        }
-    };
 
     init_build_dir()?;
     #[allow(clippy::if_same_then_else)]
@@ -166,170 +107,139 @@ fn main() -> anyhow::Result<()> {
         Some("--quiet")
     };
 
-    let formatoptcheckonly = FormatOpt {
-        // Only check, do not reformat
-        check: true,
-        ..Default::default()
-    };
+    let final_run_results = run_command(&cli.globals, cli.command, cargoarg)?;
 
-    // Default set of all packages
-    // CI always runs on all packages
-    let package = PackageOpt::default();
-    let testopts = TestOpt::default();
-    // Currently only rtic-sync supports loom tests
-    let testoptsloom = TestOpt {
-        loom: true,
-        package: PackageOpt {
-            package: Some(Package::RticSync),
-        },
-    };
+    handle_results(globals, final_run_results).map_err(|_| anyhow::anyhow!("Commands failed"))
+}
 
-    let final_run_results = match &cli.command {
-        Commands::AllCi(args) => {
-            // TODO: Reduce code duplication and repetition
-            let mut results = cargo_format(globals, &cargoarg, &formatoptcheckonly);
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
+fn run_command(
+    globals: &Globals,
+    command: Commands,
+    cargoarg: Option<&str>,
+) -> anyhow::Result<Vec<FinalRunResult>> {
+    let rtic_sync = PackageOpt::rtic_sync();
+    let cargoarg = cargoarg.map(|v| v.to_owned());
 
-            results.append(&mut cargo_clippy(globals, &cargoarg, &package, backend));
+    let results = match command {
+        Commands::Ci { skip_qemu } => {
+            use Commands::*;
 
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
+            // Test command skips RTIC for RISCV32: the UI tests do not work. We
+            // do still run clippy on them.
+            let test = Package::all()
+                .filter(|p| {
+                    p.target()
+                        .is_none_or(|t| t != RISCV32IMAC && t != RISCV32IMC)
+                })
+                .map(|p| Test(PackageOpt::one(p)));
 
-            results.append(&mut cargo(
-                globals,
-                BuildOrCheck::Check,
-                &cargoarg,
-                &package,
-                backend,
-            ));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-            results.append(&mut cargo(
-                globals,
-                BuildOrCheck::Build,
-                &cargoarg,
-                &package,
-                backend,
-            ));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
+            let mut commands = vec![
+                Format(FormatOpt { check: true }),
+                ExampleBuild {
+                    examples: ExampleArgs::default(),
+                    selector: OptionalPlatformSelector::all(),
+                },
+                Clippy(PackageOpt::all()),
+                TestLoom,
+                Doc(ExtraArguments::default()),
+                // This one is hard to get right.
+                // Book(Default::default()),
+            ];
 
-            results.append(&mut cargo_example(
-                globals,
-                BuildOrCheck::Check,
-                &cargoarg,
-                platform,
-                backend,
-                &examples_to_run,
-            ));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-            results.append(&mut cargo_example(
-                globals,
-                BuildOrCheck::Build,
-                &cargoarg,
-                platform,
-                backend,
-                &examples_to_run,
-            ));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-            results.append(&mut qemu_run_examples(
-                globals,
-                &cargoarg,
-                platform,
-                backend,
-                &examples_to_run,
-                false,
-            ));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
+            if !skip_qemu {
+                commands.extend([
+                    Qemu(QemuAndRun {
+                        examples: ExampleArgs::default(),
+                        target: Platform::Lm3s6965 {
+                            backend: argument_parsing::Lm3s6965Backend::Thumbv7,
+                        },
+                        overwrite_expected: false,
+                    }),
+                    Qemu(QemuAndRun {
+                        examples: ExampleArgs::default(),
+                        target: Platform::Lm3s6965 {
+                            backend: argument_parsing::Lm3s6965Backend::Thumbv6,
+                        },
+                        overwrite_expected: false,
+                    }),
+                    Qemu(QemuAndRun {
+                        examples: ExampleArgs::default(),
+                        target: Platform::Hifive1 {
+                            backend: Hifive1Backend::Clint,
+                        },
+                        overwrite_expected: false,
+                    }),
+                    Qemu(QemuAndRun {
+                        examples: ExampleArgs::default(),
+                        target: Platform::Hifive1 {
+                            backend: Hifive1Backend::Mecall,
+                        },
+                        overwrite_expected: false,
+                    }),
+                ]);
             }
 
-            results.append(&mut cargo_doc(globals, &cargoarg, backend, &None));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-            results.append(&mut cargo_test(globals, &testopts, backend));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-            results.append(&mut cargo_test(globals, &testoptsloom, backend));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-            results.append(&mut cargo_book(globals, &None));
-            if args.failearly {
-                return handle_results(globals, results)
-                    .map_err(|_| anyhow::anyhow!("Commands failed"));
-            }
-
-            results
+            commands.into_iter().chain(test).try_fold(
+                Vec::new(),
+                |mut acc, cmd| match run_command(globals, cmd, cargoarg.as_deref()) {
+                    Ok(mut v) => {
+                        acc.append(&mut v);
+                        Ok(acc)
+                    }
+                    Err(e) => Err(e),
+                },
+            )?
         }
-        Commands::Format(formatopts) => cargo_format(globals, &cargoarg, formatopts),
-        Commands::Clippy(packageopts) => cargo_clippy(globals, &cargoarg, packageopts, backend),
-        Commands::Check(args) => cargo(globals, BuildOrCheck::Check, &cargoarg, args, backend),
-        Commands::Build(args) => cargo(globals, BuildOrCheck::Build, &cargoarg, args, backend),
-        Commands::ExampleCheck => cargo_example(
+        Commands::Format(formatopts) => cargo_format(globals, cargoarg, &formatopts)?,
+        Commands::Clippy(packageopts) => cargo_clippy(globals, cargoarg, packageopts),
+        Commands::Check(args) => cargo(globals, BuildOrCheck::Check, cargoarg, args),
+        Commands::Build(args) => cargo(globals, BuildOrCheck::Build, cargoarg, args),
+        Commands::ExampleCheck {
+            examples: args,
+            selector,
+        } => cargo_example(
             globals,
             BuildOrCheck::Check,
-            &cargoarg,
-            platform,
-            backend,
-            &examples_to_run,
+            cargoarg,
+            selector.platforms(),
+            args,
         ),
-        Commands::ExampleBuild => cargo_example(
+        Commands::ExampleBuild {
+            examples: args,
+            selector,
+        } => cargo_example(
             globals,
             BuildOrCheck::Build,
-            &cargoarg,
-            platform,
-            backend,
-            &examples_to_run,
+            cargoarg,
+            selector.platforms(),
+            args,
         ),
         Commands::Size(args) => {
             // x86_64 target not valid
             build_and_check_size(
                 globals,
-                &cargoarg,
-                platform,
-                backend,
-                &examples_to_run,
+                cargoarg,
+                args.target.platforms(),
+                &args.examples,
                 args.overwrite_expected,
-                &args.arguments,
+                &args.arguments.rest,
             )
         }
         Commands::Qemu(args) | Commands::Run(args) => {
             // x86_64 target not valid
             qemu_run_examples(
                 globals,
-                &cargoarg,
-                platform,
-                backend,
-                &examples_to_run,
+                cargoarg,
+                args.target,
+                args.examples.get_examples(&args.target)?,
                 args.overwrite_expected,
             )
         }
-        Commands::Doc(args) => cargo_doc(globals, &cargoarg, backend, &args.arguments),
-        Commands::Test(args) => cargo_test(globals, args, backend),
-        Commands::Book(args) => cargo_book(globals, &args.arguments),
+        Commands::Doc(args) => cargo_doc(globals, cargoarg, &args.rest),
+        Commands::Test(args) => cargo_test(globals, args, false),
+        Commands::TestLoom => cargo_test(globals, rtic_sync, true),
+        Commands::Book(args) => cargo_book(globals, &args.rest),
     };
 
-    handle_results(globals, final_run_results).map_err(|_| anyhow::anyhow!("Commands failed"))
+    Ok(results)
 }
