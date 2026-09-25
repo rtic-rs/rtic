202026f2f8e74fddac433a56ecb8ea3a2fd8d78b
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2026-09-25 13:58:35 +0200

    Update blinking frequency with the user button

--- examples/embassy-stm32u5/README.md
+++ examples/embassy-stm32u5/README.md
@@ -1,6 +1,6 @@
 # STM32U5 RTIC Blink example
 
-Working example of simple LED blinking application for the NUCLEO-U575ZI-Q development board using [embassy-stm32](https://crates.io/crates/embassy-stm32) HAL. This example shows how to configure the system ticker and how to use a custom linker file.
+Working example of simple LED blinking application for the NUCLEO-U575ZI-Q development board using [embassy-stm32](https://crates.io/crates/embassy-stm32) HAL where the blinking frequency can be tuned using the user button. This example shows how to configure the system ticker and how to use a custom linker file.
 
 ## How-to
 
--- examples/embassy-stm32u5/src/bin/blinky.rs
+++ examples/embassy-stm32u5/src/bin/blinky.rs
@@ -3,10 +3,18 @@
 #![no_main]
 #![no_std]
 
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::exti;
+use embassy_stm32::gpio;
+use embassy_stm32::{bind_interrupts, interrupt};
+use panic_probe as _;
 use rtic::app;
 use rtic_monotonics::systick::prelude::*;
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(
+    pub struct Irqs{
+        EXTI13 => exti::InterruptHandler<interrupt::typelevel::EXTI13>;
+});
 
 systick_monotonic!(Mono, 1_000);
 
@@ -15,12 +23,16 @@ pub mod pac {
     pub use embassy_stm32::pac::*;
 }
 
+// Peripherals shall be false when using with embassy-stm32 HAL
+// SPI1 is just an unused interrupt needed by RTIC to dispatch all tasks
 #[app(device = pac, peripherals = false, dispatchers = [SPI1])]
 mod app {
     use super::*;
 
     #[shared]
-    struct Shared {}
+    struct Shared {
+        blinking_period: u32,
+    }
 
     #[local]
     struct Local {
@@ -36,23 +48,43 @@ mod app {
         // MSIS (4MHz) -> AHBP (DIV4)
         Mono::start(cx.core.SYST, 1_000_000);
 
-        let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+        let button = exti::ExtiInput::new(p.PC13, p.EXTI13, gpio::Pull::Down, Irqs);
+        increase_blinking_period::spawn(button).ok();
+
+        let mut led = gpio::Output::new(p.PB7, gpio::Level::High, gpio::Speed::Low);
         led.set_high();
 
         // Schedule the blinking task
         blink::spawn(led).ok();
 
         defmt::info!("Let's go!");
-        (Shared {}, Local { counts: 0 })
+        (
+            Shared {
+                blinking_period: 500,
+            },
+            Local { counts: 0 },
+        )
     }
 
-    #[task(local = [counts], priority = 1)]
-    async fn blink(cx: blink::Context, mut led: Output<'static>) {
+    #[task(shared = [blinking_period], local = [counts], priority = 1)]
+    async fn blink(mut cx: blink::Context, mut led: gpio::Output<'static>) {
         loop {
             led.toggle();
             *cx.local.counts += 1;
-            defmt::info!("blink ({})", *cx.local.counts);
-            Mono::delay(1000.millis()).await;
+            defmt::info!("blink ({})", cx.local.counts);
+            let period = cx.shared.blinking_period.lock(|val| *val);
+            Mono::delay(period.millis()).await;
+        }
+    }
+
+    #[task(shared = [blinking_period], priority = 1)]
+    async fn increase_blinking_period(
+        mut cx: increase_blinking_period::Context,
+        mut button: exti::ExtiInput<'static, embassy_stm32::mode::Async>,
+    ) {
+        loop {
+            button.wait_for_falling_edge().await;
+            cx.shared.blinking_period.lock(|val| *val += 100);
         }
     }
 }
